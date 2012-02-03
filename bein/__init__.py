a98ad68839f8fa326252e5355600bd4578b6d33d
Lucas Sinclair <lucas.sinclair@me.com>
2012-02-03 18:08:34 +0100

    Changing order of variable declaration

--- bein/__init__.py
+++ bein/__init__.py
@@ -96,6 +96,99 @@ def unique_filename_in(path=None):
             break
     return filename
 
+
+################################################################################
+class Execution(object):
+    """``Execution`` objects hold the state of a current running execution.
+
+    You should generally use the execution function below to create an
+    Execution, since it sets up the working directory properly.
+
+    Executions are run against a particular MiniLIMS object where it
+    records all the information onf programs that were run during it,
+    fetches files from it, and writes files back to it.
+
+    The important methods for the user to know are ``add`` and ``use``.
+    Everything else is used internally by bein.  ``add`` puts a file
+    into the LIMS repository from the execution's working directory.
+    ``use`` fetches a file from the LIMS repository into the working
+    directory.
+    """
+    def __init__(self, lims, working_directory):
+        self.lims = lims
+        self.working_directory = working_directory
+        self.programs = []
+        self.files = []
+        self.used_files = []
+        self.started_at = int(time.time())
+        self.finished_at = None
+        self.id = None
+
+    def path_to_file(self, id_or_alias):
+        """Fetch the path to *id_or_alias* in the attached LIMS."""
+        if self.lims == None:
+            raise ValueError("Cannot use path_to_file; no attached LIMS.")
+        else:
+            return self.lims.path_to_file(id_or_alias)
+
+    def report(self, program):
+        """Add a ProgramOutput object to the execution.
+
+        When the Execution finishes, all programs added to the
+        Execution with 'report', in the order the were added, are
+        written into the MiniLIMS repository.
+        """
+        self.programs.append(program)
+
+    def add(self, filename, description="", associate_to_id=None,
+            associate_to_filename=None, template=None, alias=None):
+        """Add a file to the MiniLIMS object from this execution.
+
+        filename is the name of the file in the execution's working
+        directory to import.  description is an optional argument to
+        assign a string or a dictionary to describe that file in the MiniLIMS
+        repository.
+
+        Note that the file is not actually added to the repository
+        until the execution finishes.
+        """
+        if isinstance(description,dict): description=str(description)
+        if filename == None:
+            if description == "":
+                raise(IOError("Tried to add None to repository."))
+            else:
+                raise(IOError("Tried to add None to repository, with description '" + description +"' ."))
+        elif not(os.path.exists(filename)):
+            raise IOError("No such file or directory: '"+filename+"'")
+        else:
+            self.files.append((filename,description,associate_to_id,
+                               associate_to_filename,template,alias))
+    def finish(self):
+        """Set the time when the execution finished."""
+        self.finished_at = int(time.time())
+
+    def use(self, file_or_alias):
+        """Fetch a file from the MiniLIMS repository.
+
+        fileid should be an integer assigned to a file in the MiniLIMS
+        repository, or a string giving a file alias in the MiniLIMS
+        repository.  The file is copied into the execution's working
+        directory with a unique filename.  'use' returns the unique
+        filename it copied the file into.
+        """
+        fileid = self.lims.resolve_alias(file_or_alias)
+        try:
+            filename = [x for (x,) in
+                        self.lims.db.execute("select exportfile(?,?)",
+                                             (fileid, self.working_directory))][0]
+            for (f,t) in self.lims.associated_files_of(fileid):
+                self.lims.db.execute("select exportfile(?,?)",
+                                     (f, os.path.join(self.working_directory,t % filename)))
+            self.used_files.append(fileid)
+            return filename
+        except ValueError, v:
+            raise ValueError("Tried to use a nonexistent file id " + str(fileid))
+
 ################################################################################
 class program(object):
     """Decorator to wrap external programs for use by bein.
@@ -440,98 +533,6 @@ class program(object):
         a = threading.Thread(target=g)
         a.start()
         return(f)
-
-################################################################################
-class Execution(object):
-    """``Execution`` objects hold the state of a current running execution.
-
-    You should generally use the execution function below to create an
-    Execution, since it sets up the working directory properly.
-
-    Executions are run against a particular MiniLIMS object where it
-    records all the information onf programs that were run during it,
-    fetches files from it, and writes files back to it.
-
-    The important methods for the user to know are ``add`` and ``use``.
-    Everything else is used internally by bein.  ``add`` puts a file
-    into the LIMS repository from the execution's working directory.
-    ``use`` fetches a file from the LIMS repository into the working
-    directory.
-    """
-    def __init__(self, lims, working_directory):
-        self.lims = lims
-        self.working_directory = working_directory
-        self.programs = []
-        self.files = []
-        self.used_files = []
-        self.started_at = int(time.time())
-        self.finished_at = None
-        self.id = None
-
-    def path_to_file(self, id_or_alias):
-        """Fetch the path to *id_or_alias* in the attached LIMS."""
-        if self.lims == None:
-            raise ValueError("Cannot use path_to_file; no attached LIMS.")
-        else:
-            return self.lims.path_to_file(id_or_alias)
-
-    def report(self, program):
-        """Add a ProgramOutput object to the execution.
-
-        When the Execution finishes, all programs added to the
-        Execution with 'report', in the order the were added, are
-        written into the MiniLIMS repository.
-        """
-        self.programs.append(program)
-
-    def add(self, filename, description="", associate_to_id=None,
-            associate_to_filename=None, template=None, alias=None):
-        """Add a file to the MiniLIMS object from this execution.
-
-        filename is the name of the file in the execution's working
-        directory to import.  description is an optional argument to
-        assign a string or a dictionary to describe that file in the MiniLIMS
-        repository.
-
-        Note that the file is not actually added to the repository
-        until the execution finishes.
-        """
-        if isinstance(description,dict): description=str(description)
-        if filename == None:
-            if description == "":
-                raise(IOError("Tried to add None to repository."))
-            else:
-                raise(IOError("Tried to add None to repository, with description '" + description +"' ."))
-        elif not(os.path.exists(filename)):
-            raise IOError("No such file or directory: '"+filename+"'")
-        else:
-            self.files.append((filename,description,associate_to_id,
-                               associate_to_filename,template,alias))
-    def finish(self):
-        """Set the time when the execution finished."""
-        self.finished_at = int(time.time())
-
-    def use(self, file_or_alias):
-        """Fetch a file from the MiniLIMS repository.
-
-        fileid should be an integer assigned to a file in the MiniLIMS
-        repository, or a string giving a file alias in the MiniLIMS
-        repository.  The file is copied into the execution's working
-        directory with a unique filename.  'use' returns the unique
-        filename it copied the file into.
-        """
-        fileid = self.lims.resolve_alias(file_or_alias)
-        try:
-            filename = [x for (x,) in
-                        self.lims.db.execute("select exportfile(?,?)",
-                                             (fileid, self.working_directory))][0]
-            for (f,t) in self.lims.associated_files_of(fileid):
-                self.lims.db.execute("select exportfile(?,?)",
-                                     (f, os.path.join(self.working_directory,t % filename)))
-            self.used_files.append(fileid)
-            return filename
-        except ValueError, v:
-            raise ValueError("Tried to use a nonexistent file id " + str(fileid))
 
 ################################################################################
 @contextmanager
